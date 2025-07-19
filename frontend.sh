03e0f7cb91ac6d9347fb788ba80d2ac58c4c2863
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2025-07-19 15:24:10 +0530

    shell scripts

--- frontend.sh
+++ frontend.sh
@@ -8,6 +8,8 @@ systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+
 cd /usr/share/nginx/html
+
 unzip /tmp/frontend.zip
  systemctl restart nginx
