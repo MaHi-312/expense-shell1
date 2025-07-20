d819050788c6720de36dbebcc7b47f4bd5ba4b13
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2025-07-20 07:34:14 +0530

    shell scripts

--- frontend.sh
+++ frontend.sh
@@ -2,18 +2,18 @@ source common.sh
 component=frontend
 
 echo install nginx
-dnf install nginx -y  >>$log_file
+dnf install nginx -y  &>>$log_file
 
 echo placing expense config file in nginx
-cp expense.conf /etc/nginx/default.d/expense.conf >>$log_file
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 
 echo remove old nginx content
-rm -rf /usr/share/nginx/html/* >>$log_file
+rm -rf /usr/share/nginx/html/* &>>$log_file
 
-cd /usr/share/nginx/html >>$log_file
+cd /usr/share/nginx/html &>>$log_file
 
 download_and_extract
 
 echo starting nginx services
-systemctl enable nginx >>$log_file
-systemctl restart nginx >>$log_file
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
