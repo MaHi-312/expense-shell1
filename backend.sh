4b7b8378e18c4a07ed3eb510e9f3ff00d91a5547
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2025-07-21 14:56:59 +0530

    shell scripts

--- backend.sh
+++ backend.sh
@@ -4,19 +4,35 @@ component=backend
 echo install nodejs repos
 dnf module disable nodejs -y &>>$log_file
 dnf module enable nodejs:18 -y &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+   echo success
+  else
+    echo failed
+fi
 
 echo install nodejs
 dnf install nodejs -y &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+   echo success
+  else
+    echo failed
+fi
 
 echo copy backend service file
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+   echo success
+  else
+    echo failed
+fi
 
 echo useradd application user
 useradd expense &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+   echo success
+  else
+    echo failed
+fi
 
 echo clean app content
 rm -rf /app &>>$log_file
